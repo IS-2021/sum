d31ec14fef630d0a743df22d71d36a658cf64b5a
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-11 20:02:03 +0200

    fix: remove conflicting import

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
-import { RouterLink } from 'vue-router';
-
 </script>
 
 <template>
-  <header class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6">
+  <header
+    class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6"
+  >
     <nav class="flex-grow">
       <ul class="flex items-center gap-6">
         <li>
