4ef88fa2eb1c7673308e325a5122beb304327dab
242448 Kwiecień Dominik <242448@edu.p.lodz.pl>
2024-04-14 13:25:13 +0200

    feat: add footer element

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <footer class="h-32 w-full bottom-0 border border-transparent bg-zinc-800 flex items-center px-6">
+
+  </footer>
+</template>
