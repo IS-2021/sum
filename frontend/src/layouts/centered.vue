604331652c9b9fd84b150558fba468cffbb92bb4
DominikKwiecien0 <242448@edu.p.lodz.pl>
2024-04-15 09:28:31 +0200

    feat: add centered layout

---
+++ frontend/src/layouts/centered.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import { useHead } from '@unhead/vue';
+
+useHead({
+  title: 'FoodGood',
+});
+</script>
+
+<template>
+  <Header />
+  <div class="grid items-center flex-grow min-h-screen my-10">
+    <RouterView />
+  </div>
+  <Footer />
+</template>
