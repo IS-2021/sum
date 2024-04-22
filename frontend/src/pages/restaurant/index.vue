bda63ac73ba01022c3161f15c1ee3450c762e44b
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-04-22 11:28:22 +0200

    feat: add title and star component

--- frontend/src/pages/restaurant/index.vue
+++ frontend/src/pages/restaurant/index.vue
@@ -4,11 +4,17 @@ meta:
 </route>
 
 <script setup lang="ts">
+import StarItem from '@/components/StarItem.vue';
+import { ref } from 'vue';
 
+const isFavourite = ref(false);
 </script>
 
 <template>
   <div>
-    <p>Nazwa restauracji</p>
+    <div class="flex flex-row">
+      <p class="px-4 text-4xl">Nazwa restauracji</p>
+      <StarItem :isFavourite="isFavourite" />
+    </div>
   </div>
 </template>
