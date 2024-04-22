7ad182eb77e162a48a80a60963deb5874020b6f1
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-22 22:34:18 +0200

    feat: add restaurant card

---
+++ frontend/src/components/restaurants/RestaurantCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ThumbsUpIcon, ClockIcon } from 'lucide-vue-next';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
+interface RestaurantCardProps {
+  imageSrc?: string;
+  name: string;
+  rating: Number;
+  openingHours: string;
+  closingHours: string;
+}
+
+const props = defineProps<RestaurantCardProps>();
+</script>
+
+<template>
+  <article class="bg-neutral-900 text-neutral-50 rounded text-xl space-y-3 p-4">
+    <AspectRatio :ratio="16 / 9">
+      <img
+        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        alt="Photo by Drew Beamer"
+        class="rounded-md object-cover w-full h-full"
+      />
+    </AspectRatio>
+
+    <header class="flex justify-between items-center">
+      <p class="font-bold">{{ name }}</p>
+
+      <div class="flex gap-3 items-center">
+        <p>{{ rating }}%</p>
+        <ThumbsUpIcon />
+      </div>
+    </header>
+
+    <footer class="text-neutral-500">
+      <p class="flex items-center gap-3 justify-end">
+        {{ openingHours }} - {{ closingHours }}
+        <ClockIcon />
+      </p>
+    </footer>
+  </article>
+</template>
--- frontend/src/pages/restaurants/index.vue
+++ frontend/src/pages/restaurants/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
+import RestaurantCard from '@/components/restaurants/RestaurantCard.vue';
 
 useHead({
   title: 'Restaurants',
@@ -7,5 +8,16 @@ useHead({
 </script>
 
 <template>
-  <div class="max-w-screen-xl w-full grid grid-cols-4 mx-auto">Restaurants</div>
+  <div class="w-full mx-auto px-4">
+    <h1 class="font-bold text-2xl mb-10">Restauracje w mieście: Łódź</h1>
+
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <RestaurantCard
+        name="Tabu Sushi"
+        :rating="4.89"
+        opening-hours="10:00"
+        closing-hours="22:00"
+      />
+    </div>
+  </div>
 </template>
