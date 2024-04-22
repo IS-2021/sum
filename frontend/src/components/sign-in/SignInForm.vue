9bad299a18d1d8b93b000eb50a12fc4992d6e8e1
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-22 21:29:24 +0200

    refactor: streamline auth pages

---
+++ frontend/src/components/sign-in/SignInForm.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+import { useForm } from 'vee-validate';
+import { postAuthLogin } from '@/lib/api/auth/auth';
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(3),
+    password: z.string().min(6),
+  }),
+);
+
+const form = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = form.handleSubmit(async (credentials) => {
+  const res = await postAuthLogin(credentials, {
+    validateStatus: (status) => status < 500,
+  });
+
+  console.log(res);
+});
+
+const isValid = form.meta.value.valid;
+</script>
+
+<template>
+  <form @submit="onSubmit" class="space-y-4">
+    <FormField v-slot="{ componentField }" name="username">
+      <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="Username" v-bind="componentField" />
+        </FormControl>
+        <FormDescription class="sr-only">This is your Username.</FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    <FormField v-slot="{ componentField }" name="password">
+      <FormItem>
+        <FormLabel>Password</FormLabel>
+        <FormControl>
+          <Input type="password" placeholder="Password" v-bind="componentField" />
+        </FormControl>
+        <FormDescription class="sr-only"> This is your password.</FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <Button :disabled="!isValid" type="submit" class="mt-8 w-full"> Submit</Button>
+  </form>
+</template>
