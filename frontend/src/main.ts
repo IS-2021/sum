16c7e7206ff434c38513f99eaae8b170bee3fda7
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-22 21:56:33 +0200

    feat: add inter font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import './assets/main.css';
+import '@fontsource-variable/inter';
 
 import App from './App.vue';
 import router from './router';
