4c8759d699c32218fea685ddb13487cf6ec4b406
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-22 23:20:39 +0200

    feat: provide mock response

--- frontend/src/pages/restaurants/index.vue
+++ frontend/src/pages/restaurants/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
 import RestaurantCard from '@/components/restaurants/RestaurantCard.vue';
+import { getGetRestaurantsResponseMock } from '@/lib/api/restaurants/restaurants.msw';
 
 useHead({
   title: 'Restaurants',
 });
+
+const restaurants = getGetRestaurantsResponseMock();
+
+console.log(restaurants);
 </script>
 
 <template>
@@ -13,11 +18,12 @@ useHead({
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       <RestaurantCard
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
         image-src="https://media-cdn.tripadvisor.com/media/photo-s/1a/37/c1/76/crispy-kimchi-roll-tunczyk.jpg"
-        name="Tabu Sushi"
-        :rating="4.89"
-        opening-hours="10:00"
+        :name="restaurant.name"
         :rating="95"
+        opening-hours="12:00"
         closing-hours="22:00"
       />
     </div>
