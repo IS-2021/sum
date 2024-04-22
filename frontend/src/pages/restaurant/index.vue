cbebde21b0ee189c28c9ad9f71ae463444d2ff3d
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-04-22 22:43:23 +0200

    feat: add like/dislike component

--- frontend/src/pages/restaurant/index.vue
+++ frontend/src/pages/restaurant/index.vue
@@ -5,16 +5,39 @@ meta:
 
 <script setup lang="ts">
 import StarItem from '@/components/StarItem.vue';
+import LikeItems from '@/components/LikeItems.vue';
 import { ref } from 'vue';
 
+type UUID = string;
+
+interface Restaurant {
+  address: UUID,
+  id: UUID,
+  isActive: boolean,
+  name: string,
+  phoneNumber: string,
+  userId: UUID,
+  isFavourite: boolean
+}
+
+const props = defineProps<{
+  restaurant: Restaurant
+}>();
+
 const isFavourite = ref(false);
+
 </script>
 
 <template>
-  <div>
-    <div class="flex flex-row">
-      <p class="px-4 text-4xl">Nazwa restauracji</p>
-      <StarItem :isFavourite="isFavourite" />
+  <div class="container">
+    <div class="flex flex-col">
+      <div class="flex flex-row">
+        <p class="pr-4 text-4xl">Nazwa restauracji</p>
+        <StarItem :isFavourite="isFavourite" />
+        <div class="flex-grow"></div>
+        <LikeItems />
+      </div>
+      <p class="flex">Address</p>
     </div>
   </div>
 </template>
