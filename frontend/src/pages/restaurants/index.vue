01f111a009418132f96ee3c3e3872b7ba5662c28
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-26 21:32:54 +0200

    feat: fetch restaurants from api

--- frontend/src/pages/restaurants/index.vue
+++ frontend/src/pages/restaurants/index.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
+import { useGetRestaurants } from '@/lib/api/restaurants/restaurants';
+import { unref } from 'vue';
 import RestaurantCard from '@/components/restaurants/RestaurantCard.vue';
-import { getGetRestaurantsResponseMock } from '@/lib/api/restaurants/restaurants.msw';
 
 useHead({
   title: 'Restaurants',
 });
 
-const restaurants = getGetRestaurantsResponseMock();
-
-console.log(restaurants);
+const { data, isPending: areRestaurantsLoading } = useGetRestaurants();
+const restaurants = unref(data)?.data;
 </script>
 
 <template>
-  <div class="w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-xl mx-auto px-4">
-    <h1 class="font-bold text-2xl mb-8">Restauracje w mieście: Łódź</h1>
+  <template v-if="areRestaurantsLoading">
+    <p>Loading...</p>
+  </template>
+  <template v-else-if="restaurants">
+    <div class="w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-xl mx-auto px-4">
+      <h1 class="font-bold text-2xl mb-8">Restauracje w mieście: Łódź</h1>
 
-    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      <RestaurantCard
-        v-for="restaurant in restaurants"
-        :key="restaurant.id"
-        image-src="https://media-cdn.tripadvisor.com/media/photo-s/1a/37/c1/76/crispy-kimchi-roll-tunczyk.jpg"
-        :name="restaurant.name"
-        :rating="95"
-        opening-hours="12:00"
-        closing-hours="22:00"
-      />
+      <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <RestaurantCard
+          v-for="restaurant in restaurants"
+          :key="restaurant.id"
+          :id="restaurant.id"
+          :image-src="restaurant.imageUrl"
+          :name="restaurant.name"
+          :rating="95"
+          opening-hours="12:00"
+          closing-hours="22:00"
+        />
+      </div>
     </div>
-  </div>
+  </template>
 </template>
