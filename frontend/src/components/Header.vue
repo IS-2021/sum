321f5117eb4db6f82fa8aeb03943376a7c5a3883
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-27 15:05:49 +0200

    fix: fix navbar z-index

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 
 <template>
   <header
-    class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6"
+    class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6 z-50"
   >
     <nav class="flex-grow">
       <ul class="flex items-center gap-6">
