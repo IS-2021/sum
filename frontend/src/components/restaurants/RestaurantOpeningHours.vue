ed6f6383a43e6f8eebd9bd91a5071f5a3f034e7c
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-27 16:06:21 +0200

    feat: add opening hours hover card

---
+++ frontend/src/components/restaurants/RestaurantOpeningHours.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { HoursDTO } from '@/lib/api-model';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
+import { ClockIcon } from 'lucide-vue-next';
+import { ref } from 'vue';
+
+const props = defineProps<{ hours: HoursDTO }>();
+
+const currentDayHours = Object.entries(props.hours).find(
+  ([day]) => day === new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase(),
+);
+
+let todayOpeningHours = ref('');
+if (currentDayHours && currentDayHours[1]) {
+  const [openingHours, closingHours] = currentDayHours[1];
+  todayOpeningHours.value = `${openingHours} - ${closingHours}`;
+} else {
+  todayOpeningHours.value = 'Closed';
+}
+</script>
+
+<template>
+  <HoverCard>
+    <HoverCardTrigger>
+      <p class="flex items-center gap-2 justify-end">
+        <span class="leading-5">{{ todayOpeningHours }}</span>
+        <ClockIcon class="w-5 h-5" />
+      </p>
+    </HoverCardTrigger>
+    <HoverCardContent>
+      <ul>
+        <li v-for="hours in Object.entries(props.hours)">{{ hours[0] }} - {{ hours[1] }}</li>
+      </ul>
+    </HoverCardContent>
+  </HoverCard>
+</template>
