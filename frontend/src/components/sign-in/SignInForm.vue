66983c53bd6e97ce982b2eb06e9fe5c8a3dba7fd
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-04-30 11:17:16 +0200

    feat: redirect to homepage on success

--- frontend/src/components/sign-in/SignInForm.vue
+++ frontend/src/components/sign-in/SignInForm.vue
@@ -17,6 +17,9 @@ import { ref } from 'vue';
 import type { ValidationFailed422Response } from '@/lib/api-model';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { AlertCircleIcon } from 'lucide-vue-next';
+import { useRouter } from 'vue-router/auto';
+
+const router = useRouter();
 
 const formSchema = toTypedSchema(
   z.object({
@@ -37,6 +40,7 @@ const onSubmit = form.handleSubmit(async (credentials) => {
 
   if (res.status === 200) {
     errorMessage.value = '';
+    await router.push('/');
   } else if (res.status === 400) {
     const { message } = res.data as unknown as ValidationFailed422Response;
 
