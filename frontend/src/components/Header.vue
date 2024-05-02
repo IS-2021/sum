6ca008ca09b533fcd38596bb6b3a67cc4fead572
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-05-02 13:45:02 +0200

    feat: add logo in header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,11 +4,16 @@ import { postLogout } from '@/lib/api/auth/auth';
 </script>
 
 <template>
-  <header
-    class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6 z-50"
-  >
+  <header class="h-16 w-full top-0 border border-transparent bg-zinc-800 sticky flex items-center px-6 z-50">
     <nav class="flex-grow">
       <ul class="flex items-center gap-6">
+        <li>
+          <div class="h-8 w-8">
+            <RouterLink to="/">
+              <img src="@/assets/logo.svg" alt="Logo">
+            </RouterLink>
+          </div>
+        </li>
         <li>
           <RouterLink to="/">Home</RouterLink>
         </li>
