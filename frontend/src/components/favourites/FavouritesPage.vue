82ba11157dc4c80eae225beb46e6fe71bba2cabb
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-03 22:31:38 +0200

    feat: add router link

--- frontend/src/components/favourites/FavouritesPage.vue
+++ frontend/src/components/favourites/FavouritesPage.vue
@@ -2,9 +2,10 @@
 import type { UserDTO } from '@/lib/api-model';
 import { useGetUsersIdFavourites } from '@/lib/api/favourites/favourites';
 import { computed, unref } from 'vue';
-import { GripVerticalIcon, StarIcon } from 'lucide-vue-next';
+import { GripVerticalIcon, StarIcon, ArrowRightIcon } from 'lucide-vue-next';
 import { getImageUrl } from '@/lib/assets';
 import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
 const { user } = defineProps<{ user: UserDTO }>();
 
@@ -33,19 +34,32 @@ const hasAnyFavourites = computed(() => (favourites.value ? favourites.value.len
         v-for="favourite in favourites"
         :key="favourite.id"
         :style="`--image-url: url('${getImageUrl(favourite.imageUrl)}')`"
-        :class="cn('rounded bg-cover', `bg-[image:var(--image-url)]`)"
+        :class="cn('rounded bg-cover group', `bg-[image:var(--image-url)]`)"
       >
         <div
-          class="flex items-center gap-4 px-5 py-3 transition-all duration-200 bg-opacity-50 hover:bg-opacity-25 bg-neutral-900"
+          class="flex items-center gap-4 px-5 py-3 transition-all duration-200 bg-opacity-50 hover:bg-opacity-35 bg-neutral-900"
         >
           <GripVerticalIcon class="cursor-move" />
-          <Button size="icon" variant="ghost">
+          <Button size="icon" variant="ghost" class="hover:!bg-opacity-40">
             <StarIcon class="fill-neutral-50" />
           </Button>
-          <div class="ml-2">
-            <p class="text-lg font-semibold mb-2">{{ favourite.name }}</p>
-            <p class="text-neutral-300">Closed</p>
-          </div>
+
+          <RouterLink :to="`/restaurant/${favourite.id}`" class="flex-grow">
+            <div class="ml-2">
+              <p class="text-lg font-semibold mb-2">{{ favourite.name }}</p>
+              <p class="text-neutral-300">Closed</p>
+            </div>
+          </RouterLink>
+
+          <Button
+            as-child
+            variant="ghost"
+            class="group-hover:opacity-100 opacity-0 transition-all hover:!bg-opacity-40 hidden md:block"
+          >
+            <RouterLink :to="`/restaurant/${favourite.id}`">
+              Open <ArrowRightIcon class="inline-block w-5 h-5 ml-2" />
+            </RouterLink>
+          </Button>
         </div>
       </li>
     </ul>
