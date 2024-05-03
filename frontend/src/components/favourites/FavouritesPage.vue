d50477f18b43b5f7e9c1e78670a91a883ffb4239
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-03 23:05:55 +0200

    feat: make restaurants draggable

--- frontend/src/components/favourites/FavouritesPage.vue
+++ frontend/src/components/favourites/FavouritesPage.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
-import type { UserDTO } from '@/lib/api-model';
+import type { RestaurantDTO, UserDTO } from '@/lib/api-model';
 import { useGetUsersIdFavourites } from '@/lib/api/favourites/favourites';
-import { computed, unref } from 'vue';
-import { GripVerticalIcon, StarIcon, ArrowRightIcon } from 'lucide-vue-next';
+import { computed, ref, unref, watchEffect } from 'vue';
+import { ArrowRightIcon, GripVerticalIcon, StarIcon } from 'lucide-vue-next';
 import { getImageUrl } from '@/lib/assets';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
+import { VueDraggable } from 'vue-draggable-plus';
 
 const { user } = defineProps<{ user: UserDTO }>();
 
 const { data, isPending } = useGetUsersIdFavourites(unref(user)?.id, {
   query: {
     enabled: !!unref(user)?.id,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
   },
 });
-const favourites = computed(() => data.value?.data);
-const hasAnyFavourites = computed(() => (favourites.value ? favourites.value.length > 0 : false));
+
+const favourites = ref<RestaurantDTO[]>([]);
+const hasAnyFavourites = computed(() => favourites.value.length > 0);
+
+watchEffect(() => {
+  if (data.value && data.value.data.length > 0) {
+    favourites.value = data.value.data;
+  }
+});
 </script>
 
 <template>
@@ -28,18 +38,18 @@ const hasAnyFavourites = computed(() => (favourites.value ? favourites.value.len
     </p>
   </div>
 
-  <div>
-    <ul class="space-y-4">
+  <div v-if="favourites">
+    <VueDraggable class="space-y-4" v-model="favourites" handle=".handle" :animation="250">
       <li
         v-for="favourite in favourites"
         :key="favourite.id"
         :style="`--image-url: url('${getImageUrl(favourite.imageUrl)}')`"
-        :class="cn('rounded bg-cover group', `bg-[image:var(--image-url)]`)"
+        :class="cn('rounded bg-cover group list-none', `bg-[image:var(--image-url)]`)"
       >
         <div
           class="flex items-center gap-4 px-5 py-3 transition-all duration-200 bg-opacity-50 hover:bg-opacity-35 bg-neutral-900"
         >
-          <GripVerticalIcon class="cursor-move" />
+          <GripVerticalIcon class="handle cursor-move" />
           <Button size="icon" variant="ghost" class="hover:!bg-opacity-40">
             <StarIcon class="fill-neutral-50" />
           </Button>
@@ -62,6 +72,6 @@ const hasAnyFavourites = computed(() => (favourites.value ? favourites.value.len
           </Button>
         </div>
       </li>
-    </ul>
+    </VueDraggable>
   </div>
 </template>
