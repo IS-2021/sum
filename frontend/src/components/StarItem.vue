e388bb363bc686c6130ca40c4449f7a1d82d02f7
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-05-05 23:20:24 +0200

    feat: save favourite restaurant on backend

--- frontend/src/components/StarItem.vue
+++ frontend/src/components/StarItem.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
+import type { Uuid } from '@/lib/api-model';
+import { postUsersDeleteFavourites, postUsersIdFavourites } from '@/lib/api/favourites/favourites';
+import { useGetUsersMe } from '@/lib/api/users/users';
 import { Star } from 'lucide-vue-next';
-import { ref } from 'vue';
-import type { Ref } from 'vue';
+import { ref, unref, computed } from 'vue';
 
 const props = defineProps<{
-  isFavourite: Boolean
-}>()
+  isFavourite: Boolean;
+  restaurantId: Uuid;
+}>();
 
 const emit = defineEmits<{
-  (e: 'favouriteChange', isFavourite: Boolean): void
-}>()
+  (e: 'favouriteChange', isFavourite: Boolean): void;
+}>();
 
 const isFavourite = ref(props.isFavourite);
 
 const toggleFavourite = () => {
+  const { data } = useGetUsersMe();
+  const user = computed(() => unref(data)?.data);
   isFavourite.value = !isFavourite.value;
   emit('favouriteChange', isFavourite.value);
-}
-
+  if (isFavourite.value === true && user.value) {
+    postUsersIdFavourites(user.value.id, { restaurantId: props.restaurantId });
+  } else if (isFavourite.value === false && user.value) {
+    postUsersDeleteFavourites({ restaurantIds: [props.restaurantId], userId: user.value.id });
+  }
+};
 </script>
 
 <template>
