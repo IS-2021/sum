053c622776a8aed4d3aaf284818bd613e1272684
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-05-06 14:15:47 +0200

    refactor: move logic to parent & add event listener

--- frontend/src/components/StarItem.vue
+++ frontend/src/components/StarItem.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import type { Uuid } from '@/lib/api-model';
-import { postUsersDeleteFavourites, postUsersIdFavourites } from '@/lib/api/favourites/favourites';
-import { useGetUsersMe } from '@/lib/api/users/users';
 import { Star } from 'lucide-vue-next';
-import { ref, unref, computed } from 'vue';
-
-const { data } = useGetUsersMe();
-const user = computed(() => unref(data)?.data);
+import { ref } from 'vue';
+import { Button } from './ui/button';
 
 const props = defineProps<{
   isFavourite: Boolean;
-  restaurantId: Uuid;
 }>();
 
 const emit = defineEmits<{
@@ -22,15 +16,14 @@ const isFavourite = ref(props.isFavourite);
 const toggleFavourite = () => {
   isFavourite.value = !isFavourite.value;
   emit('favouriteChange', isFavourite.value);
-  if (isFavourite.value === true && user.value) {
-    postUsersIdFavourites(user.value.id, { restaurantId: props.restaurantId });
-  } else if (isFavourite.value === false && user.value) {
-    postUsersDeleteFavourites({ restaurantIds: [props.restaurantId], userId: user.value.id });
-  }
 };
 </script>
 
 <template>
-  <Star v-if="isFavourite" fill="white" class="w-7 h-7 cursor-pointer" @click="toggleFavourite" />
-  <Star v-else class="w-7 h-7 cursor-pointer" @click="toggleFavourite" />
+  <Button v-if="isFavourite" size="icon" variant="ghost" class="hover:!bg-opacity-40">
+    <Star fill="white" class="w-7 h-7 cursor-pointer" @click="toggleFavourite" />
+  </Button>
+  <Button v-else size="icon" variant="ghost" class="hover:!bg-opacity-40">
+    <Star class="w-7 h-7 cursor-pointer" @click="toggleFavourite" />
+  </Button>
 </template>
