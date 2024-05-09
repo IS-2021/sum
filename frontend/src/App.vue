b66c8d1109be26a3d1bd80e3162e340d92e6621b
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-09 12:52:59 +0200

    fix: remove dark mode class

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // This is soo hacky, but I haven't found out why the class="dark" is attached to the html tag
+  // after the JS is loaded. This is a workaround to remove the class after the JS is loaded.
+  setTimeout(() => {
+    document.documentElement.classList.remove('dark');
+  }, 10);
+});
 </script>
 
 <template>
