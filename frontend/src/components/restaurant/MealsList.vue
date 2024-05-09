7e76909021dd722872268d99679e62c0c8229551
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-09 18:36:17 +0200

    fix: fix restaurant view

--- frontend/src/components/restaurant/MealsList.vue
+++ frontend/src/components/restaurant/MealsList.vue
@@ -18,13 +18,14 @@ const amount = ref(0);
 
 const categories = ref(props.categories);
 </script>
+
 <template>
   <template v-if="areMealsLoading">
     <p>Loading...</p>
   </template>
   <h1 class="text-2xl mt-16 font-bold">Available packages</h1>
   <div class="flex gap-12 mt-4">
-    <div class="w-96 space-y-3 p-4 bg-neutral-900 rounded h-fit">
+    <div class="w-96 space-y-3 p-4 bg-neutral-200 rounded h-fit">
       <p>Excluding dishes that contain:</p>
       <TagsInput />
       <Filters />
@@ -36,14 +37,14 @@ const categories = ref(props.categories);
     >
       <p v-if="meals?.length === 0">No meals found</p>
       <div v-else v-for="meal in meals" v-bind:key="meal.id">
-        <div class="bg-neutral-900 min-w-96 rounded p-4 space-y-3">
+        <div class="bg-neutral-200 min-w-96 rounded p-4 space-y-3">
           <div class="flex">
             <h1 class="font-semibold text-xl">{{ meal.name }}</h1>
             <div class="flex-grow"></div>
             <p class="text-xs">Available amount: {{ amount }}</p>
           </div>
-          <p class="text-neutral-300">{{ meal.description }}</p>
-          <p class="text-neutral-300">Ingredients</p>
+          <p class="text-neutral-950">{{ meal.description }}</p>
+          <p class="text-neutral-800">Ingredients</p>
           <Button class="w-1/2">Book</Button>
         </div>
       </div>
