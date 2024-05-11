51924acb2fa81cc93b75d7c945c49c8a9e1a8ad4
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-11 23:40:11 +0200

    feat: add active link

---
+++ frontend/src/components/AppNavLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { RouterLinkProps as AutoRouterLinkProps } from 'vue-router/auto';
+import type { RouterLinkProps } from 'vue-router';
+
+type AppRouterLinkProps = Omit<RouterLinkProps, 'to' | 'activeClass' | 'exactActiveClass'> & {
+  to: (typeof AutoRouterLinkProps)['to'];
+};
+
+const props = defineProps<AppRouterLinkProps>();
+</script>
+
+<template>
+  <RouterLink :to="props.to" exact-active-class="text-primary">
+    <slot />
+  </RouterLink>
+</template>
