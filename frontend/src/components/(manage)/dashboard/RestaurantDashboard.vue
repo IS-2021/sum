a0105832e733fde69bd61653bbe527f749b9c3aa
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-20 20:29:23 +0200

    fix: bootstrap manage panel markup

---
+++ frontend/src/components/(manage)/dashboard/RestaurantDashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+
+useHead({
+  title: 'Restaurant Dashboard',
+});
+</script>
+
+<template>
+  <h1 class="text-2xl font-semibold tracking-tight mb-10">
+    Some local restaurant name that is kinda long
+  </h1>
+</template>
---
+++ frontend/src/layouts/manage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+import Navbar from '@/components/(manage)/common/Navbar.vue';
+
+useHead({
+  title: 'FoodGood',
+});
+</script>
+
+<template>
+  <div class="flex flex-grow">
+    <Navbar />
+    <div class="flex-grow mx-16 mt-14">
+      <RouterView />
+    </div>
+  </div>
+</template>
