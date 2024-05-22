b8365c7d03ce0041fff24bbd72961e5c4f268345
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-22 19:57:27 +0200

    feat: add other panel pages

---
+++ frontend/src/pages/manage/reports/index.vue
@@ -0,0 +1,12 @@
+<route lang="yaml">
+meta:
+  layout: manage
+</route>
+
+<script setup lang="ts">
+import ReportsPage from '@/components/(manage)/reports/ReportsPage.vue';
+</script>
+
+<template>
+  <ReportsPage />
+</template>
