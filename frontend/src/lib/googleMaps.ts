5157e1f5a95e639dc97eef7a729ddcbf4f1cac94
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-23 15:24:22 +0200

    Revert "feat: integrate places api searchbox"
    This reverts commit 12561a5453ff5154fab7af1a55a879988bc089b7.

--- frontend/src/lib/googleMaps.ts
+++
@@ -1,9 +0,0 @@
-import { Loader } from '@googlemaps/js-api-loader';
-
-const loader = new Loader({
-  apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
-  version: 'weekly',
-  libraries: ['places'],
-});
-
-export const placesApi = await loader.importLibrary('places');
