501ebe156b5fed653d7e4c7b90c4763f4e01930d
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-24 12:40:21 +0200

    fix: improve navbar

--- frontend/src/components/(manage)/common/MobileNavbar.vue
+++ frontend/src/components/(manage)/common/MobileNavbar.vue
@@ -13,12 +13,6 @@ import Navbar from '@/components/(manage)/common/Navbar.vue';
     </SheetTrigger>
     <SheetContent side="left" class="max-w-80">
       <Navbar />
-
-      <SheetFooter>
-        <SheetClose as-child>
-          <Button type="submit"> Save changes </Button>
-        </SheetClose>
-      </SheetFooter>
     </SheetContent>
   </Sheet>
 </template>
--- frontend/src/components/(manage)/common/Navbar.vue
+++ frontend/src/components/(manage)/common/Navbar.vue
@@ -25,44 +25,45 @@ export default defineComponent({
 </script>
 
 <template>
-  <aside class="w-full h-svh px-6 py-5">
+  <aside class="w-full max-h-svh h-full px-6 py-5 sticky top-0 flex flex-col">
     <RouterLink to="/manage">
       <Logo class="mb-8" />
     </RouterLink>
 
-    <nav>
-      <ul class="space-y-2">
-        <li>
-          <NavLink to="/manage">
-            <LayoutDashboardIcon />
-            Dashboard
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/manage/bookings">
-            <TicketCheckIcon />
-            Bookings
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/manage/meals">
-            <CookingPotIcon />
-            Meals
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/manage/reports">
-            <MessageCircleWarningIcon />
-            Reports
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/manage/settings">
-            <SettingsIcon />
-            Restaurant settings
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div class="flex flex-col justify-between flex-grow">
+      <nav>
+        <ul class="space-y-2">
+          <li>
+            <NavLink to="/manage">
+              <LayoutDashboardIcon />
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/manage/bookings">
+              <TicketCheckIcon />
+              Bookings
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/manage/meals">
+              <CookingPotIcon />
+              Meals
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/manage/reports">
+              <MessageCircleWarningIcon />
+              Reports
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <NavLink to="/manage/settings">
+        <SettingsIcon />
+        Restaurant settings
+      </NavLink>
+    </div>
   </aside>
 </template>
