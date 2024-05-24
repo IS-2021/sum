3e12619bd32ca5d3167abc47bb06f53700324f16
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-25 00:55:56 +0200

    fix: reverse geocoding returns always one result

--- backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/geo/geocoding/GeocodingService.java
+++ backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/geo/geocoding/GeocodingService.java
@@ -8,6 +8,7 @@ import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
 import com.google.maps.model.LocationType;
 import lombok.AllArgsConstructor;
+import org.example.sumatyw_backend.exceptions.ReverseGeocodeException;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,7 +19,7 @@ public class GeocodingService {
 
     private final GeoApiContext geoApiContext;
 
-    public GeocodingResult[] reverseGeocode(double lat, double lng) throws IOException, InterruptedException, ApiException {
+    public GeocodingResult reverseGeocode(double lat, double lng) throws IOException, InterruptedException, ApiException {
         GeocodingResult[] results = GeocodingApi
             .reverseGeocode(geoApiContext, new LatLng(lat, lng))
             .language("pl")
@@ -26,7 +27,18 @@ public class GeocodingService {
             .locationType(LocationType.ROOFTOP)
             .await();
 
-        return results;
+        if (results.length == 0) {
+            results = GeocodingApi
+                .reverseGeocode(geoApiContext, new LatLng(lat, lng))
+                .language("pl")
+                .resultType(AddressType.STREET_ADDRESS)
+                .await();
+        }
+
+        if (results.length == 0) {
+            throw new ReverseGeocodeException(String.format("No address found for lat: %f, lng: %f", lat, lng));
+        }
+        return results[0];
     }
 
 }
