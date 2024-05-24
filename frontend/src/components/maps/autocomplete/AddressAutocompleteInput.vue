617b02f4ed6d924659901e798164900c51141e4d
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-24 18:58:53 +0200

    refactor: encapsulate autocompletion logic

--- frontend/src/components/maps/autocomplete/AddressAutocompleteInput.vue
+++ frontend/src/components/maps/autocomplete/AddressAutocompleteInput.vue
@@ -1,56 +1,22 @@
 <script setup lang="ts">
-import { useDebounceFn } from '@vueuse/core';
-
-import type { AutocompleteDTO } from '@/lib/api-model';
 import CitiesCombobox from '@/components/maps/autocomplete/CitiesCombobox.vue';
-import { computed, nextTick, ref, watchEffect } from 'vue';
-import { getGeoAutocomplete } from '@/lib/api/geo/geo';
-import { v4 } from 'uuid';
+import { useAddressAutocomplete } from '@/components/maps/autocomplete/useAddressAutocomplete';
 
 const emit = defineEmits<{
   (e: 'onPlaceSelect', placeId: string): void;
 }>();
 
-const completionQuery = ref<string>('');
-const sessionToken = ref<string>(v4());
-const enableQuery = computed(() => completionQuery.value.length >= 3);
-
-const cities = ref<AutocompleteDTO[]>([]);
-
-watchEffect(async () => {
-  if (enableQuery.value) {
-    const res = await getGeoAutocomplete({
-      query: completionQuery.value,
-      sessionToken: sessionToken.value,
-    });
-
-    if (res.status === 200) {
-      cities.value = res.data;
-    }
-  }
+const { cities, handlePlaceSearch, handlePlaceSelect } = useAddressAutocomplete({
+  onAddressSelect: (placeId: string) => {
+    emit('onPlaceSelect', placeId);
+  },
 });
-
-const handlePlaceSearch = async (payload: string) => {
-  if (payload.length < 2) {
-    return;
-  }
-
-  completionQuery.value = payload;
-  await nextTick();
-};
-const debouncedHandlePlaceSearch = useDebounceFn(handlePlaceSearch, 500);
-
-const handlePlaceSelect = (payload: AutocompleteDTO['placeId']) => {
-  completionQuery.value = '';
-  sessionToken.value = v4();
-  emit('onPlaceSelect', payload);
-};
 </script>
 
 <template>
   <CitiesCombobox
     :completions="cities"
-    @searchChange="debouncedHandlePlaceSearch"
+    @searchChange="handlePlaceSearch"
     @onSelect="handlePlaceSelect"
   />
 </template>
