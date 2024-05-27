ec258e7645d7c406859f6054226b71535660b528
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-27 18:44:40 +0200

    refactor: break down into smaller components

---
+++ frontend/src/components/restaurants/views/MapBrowser.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { getImageUrl } from '@/lib/assets';
+import { formatAddressShort } from '@/lib/googleMaps';
+import { ref } from 'vue';
+import type { RestaurantDTO } from '@/lib/api-model';
+import { useUser } from '@/composables/useUser';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import RestaurantsMap from '@/components/maps/restaurants/RestaurantsMap.vue';
+
+const props = defineProps<{
+  restaurants: RestaurantDTO[];
+}>();
+
+const { user } = useUser();
+const pickedRestaurant = ref<RestaurantDTO | null>(null);
+</script>
+
+<template class="overflow-hidden">
+  <RestaurantsMap
+    class="mb-10 flex-grow min-h-[500px]"
+    v-if="user?.address"
+    @onPinClick="(restaurant) => (pickedRestaurant = restaurant)"
+    :center-lat="user.address.latitude"
+    :center-lng="user.address.longitude"
+    :restaurants="restaurants"
+  />
+
+  <RouterLink :to="`/restaurant/${pickedRestaurant.id}`" v-if="pickedRestaurant">
+    <div
+      class="absolute flex gap-3 bottom-4 mx-3 px-2 py-2 left-2 right-2 rounded bg-white border border-neutral-300"
+    >
+      <div class="w-28">
+        <AspectRatio :ratio="16 / 9" class="overflow-clip rounded-md">
+          <img
+            :src="getImageUrl(pickedRestaurant.imageUrl)"
+            :alt="pickedRestaurant.name"
+            class="object-cover w-full h-full"
+          />
+        </AspectRatio>
+      </div>
+      <div>
+        <p class="font-bold mb-1">{{ pickedRestaurant.name }}</p>
+        <p class="text-sm">{{ formatAddressShort(pickedRestaurant.address) }}</p>
+      </div>
+    </div>
+  </RouterLink>
+</template>
--- frontend/src/pages/restaurants/index.vue
+++ frontend/src/pages/restaurants/index.vue
@@ -1,20 +1,20 @@
+<route lang="yaml">
+meta:
+  layout: restaurants
+</route>
+
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
 import { useGetRestaurants } from '@/lib/api/restaurants/restaurants';
-import { computed, ref, unref } from 'vue';
-import RestaurantCard from '@/components/restaurants/RestaurantCard.vue';
+import { computed, unref } from 'vue';
 import { useUser } from '@/composables/useUser';
-import RestaurantsMap from '@/components/maps/restaurants/RestaurantsMap.vue';
-import { AspectRatio } from '@/components/ui/aspect-ratio';
-import type { RestaurantDTO } from '@/lib/api-model';
-import { getImageUrl } from '@/lib/assets';
-import { formatAddressShort } from '@/lib/googleMaps';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import MapBrowser from '@/components/restaurants/views/MapBrowser.vue';
+import ListBrowser from '@/components/restaurants/views/ListBrowser.vue';
 
 useHead({
   title: 'Restaurants',
 });
-
-const pickedRestaurant = ref<RestaurantDTO | null>(null);
 
 const { user } = useUser();
 const { data, isPending: areRestaurantsLoading } = useGetRestaurants();
@@ -26,61 +26,20 @@ const restaurants = computed(() => unref(data)?.data);
     <p>Loading...</p>
   </template>
   <template v-else-if="restaurants">
-    <div class="flex flex-col w-full">
-      <h1 class="font-bold text-2xl mb-8 tracking-tight">
-        Restaurants {{ user?.address && `in ${user.address.city}` }}
-      </h1>
+    <Tabs default-value="list" class="mt-4">
+      <TabsList class="grid gap-2 grid-cols-2 px-4 sm:container">
+        <TabsTrigger value="list" class="data-[state=active]:bg-primary"> List </TabsTrigger>
+        <TabsTrigger value="map" class="data-[state=active]:bg-primary"> Map </TabsTrigger>
+      </TabsList>
 
-      <RestaurantsMap
-        class="mb-10 flex-grow min-h-screen"
-        v-if="user?.address"
-        @onPinClick="(restaurant) => (pickedRestaurant = restaurant)"
-        :center-lat="user.address.latitude"
-        :center-lng="user.address.longitude"
-        :restaurants="restaurants"
-      />
-
-      <RouterLink :to="`/restaurant/${pickedRestaurant.id}`" v-if="pickedRestaurant">
-        <div
-          class="absolute flex gap-3 bottom-4 mx-3 px-2 py-2 left-2 right-2 rounded bg-white border border-neutral-300"
-        >
-          <div class="w-28">
-            <AspectRatio :ratio="16 / 9" class="overflow-clip rounded-md">
-              <img
-                :src="getImageUrl(pickedRestaurant.imageUrl)"
-                :alt="pickedRestaurant.name"
-                class="object-cover w-full h-full"
-              />
-            </AspectRatio>
-          </div>
-          <div>
-            <p class="font-bold mb-1">{{ pickedRestaurant.name }}</p>
-            <p class="text-sm">{{ formatAddressShort(pickedRestaurant.address) }}</p>
-          </div>
+      <TabsContent value="list">
+        <div class="px-4 sm:container">
+          <ListBrowser :restaurants="restaurants" />
         </div>
-      </RouterLink>
-
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4" v-if="restaurants.length > 0">
-        <RestaurantCard
-          v-for="restaurant in restaurants"
-          :key="restaurant.id"
-          :id="restaurant.id"
-          :image-src="restaurant.imageUrl"
-          :name="restaurant.name"
-          :hours="restaurant.hours"
-          :likes="restaurant.likesCount"
-          :dislikes="restaurant.dislikesCount"
-        />
-      </div>
-      <div v-else class="flex items-center gap-10 justify-center mt-10">
-        <img src="@/assets/images/undraw_no_data_re_kwbl.svg" class="w-20" />
-        <div>
-          <p class="text-lg tracking-tight mb-2 font-semibold">
-            All restaurant menus in your area are empty
-          </p>
-          <p>Try changing the location in your profile</p>
-        </div>
-      </div>
-    </div>
+      </TabsContent>
+      <TabsContent value="map">
+        <MapBrowser :restaurants="restaurants" />
+      </TabsContent>
+    </Tabs>
   </template>
 </template>
