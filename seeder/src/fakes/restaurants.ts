db184e0cece235a9efeb87af11179c7d0eccd378
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-27 14:43:50 +0200

    fix: fix issue with landline numbers

--- seeder/src/fakes/restaurants.ts
+++ seeder/src/fakes/restaurants.ts
@@ -2,9 +2,13 @@ import { RestaurantInputDTO, Uuid } from "../lib/api-model";
 import { getRestaurants } from "../lib/api/restaurants/restaurants";
 import { logEvent } from "../lib/logger";
 import { getAdminRestaurants } from "../lib/api/admin-restaurants/admin-restaurants";
+import { fakePhoneNumber } from "./helpers";
 
 export async function addRestaurant(restaurantInput: RestaurantInputDTO) {
-  const { data, status } = await getRestaurants().postRestaurants(restaurantInput);
+  const { data, status } = await getRestaurants().postRestaurants({
+    ...restaurantInput,
+    phoneNumber: fakePhoneNumber(),
+  });
 
   if (status === 200) {
     logEvent({
