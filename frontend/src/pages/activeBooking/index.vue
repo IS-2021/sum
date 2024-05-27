9684387f672bf98910f59a81a4191271ef56d486
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-28 01:23:58 +0200

    fix: fix first render issue

--- frontend/src/pages/activeBooking/index.vue
+++ frontend/src/pages/activeBooking/index.vue
@@ -6,26 +6,43 @@ meta:
 <script setup lang="ts">
 import { useHead } from '@unhead/vue';
 
-import { getMeal, getRestaurant } from '@/components/bookings/bookings';
-
 import Button from '@/components/ui/button/Button.vue';
 import { useUser } from '@/composables/useUser';
-import { useGetBookingsActive } from '@/lib/api/bookings/bookings';
-import { computed, unref, type Ref } from 'vue';
-import type { BookingDTO } from '@/lib/api-model';
+import { getBookingsActive } from '@/lib/api/bookings/bookings';
+import { ref, watchEffect } from 'vue';
+import type { BookingDTO, MealDTO, RestaurantDTO } from '@/lib/api-model';
+import { getMealsId } from '@/lib/api/meals/meals';
+import { getRestaurantsId } from '@/lib/api/restaurants/restaurants';
 
 useHead({
   title: 'Active Booking - FoodGood',
 });
 
 const { user } = useUser();
+const activeBooking = ref<BookingDTO | null>(null);
+const meal = ref<MealDTO | null>(null);
+const restaurant = ref<RestaurantDTO | null>(null);
 
-let activeBooking: Ref<BookingDTO | undefined>;
+watchEffect(async () => {
+  if (!user.value) {
+    return;
+  }
 
-if (user.value) {
-  const { data } = useGetBookingsActive({ userId: user.value.id });
-  activeBooking = computed(() => unref(data)?.data);
-}
+  const res = await getBookingsActive({
+    userId: user.value.id,
+  });
+
+  if (res.status !== 200) {
+    throw new Error('Failed to fetch active booking');
+  }
+
+  if (res.status === 200 && res.data) {
+    activeBooking.value = res.data;
+
+    meal.value = (await getMealsId(activeBooking.value.mealId)).data;
+    restaurant.value = (await getRestaurantsId(activeBooking.value.userId)).data;
+  }
+});
 </script>
 
 <template>
@@ -33,31 +50,26 @@ if (user.value) {
     <h1 class="font-bold text-2xl mb-8 tracking-tight">Active Booking</h1>
     <div class="space-y-5 p-4 bg-neutral-100 border border-neutral-200 w-full">
       <h2 class="font-semibold">Order details</h2>
-      <p v-if="!activeBooking?.id">You don't have any active bookings.</p>
-      <ul v-if="activeBooking?.id" class="space-y-2">
+      <p v-if="!activeBooking">You don't have any active bookings.</p>
+      <ul v-if="meal && activeBooking" class="space-y-2">
         <li>
-          <p>Meal: {{ getMeal(activeBooking.mealId)?.name }}</p>
+          <p>Meal: {{ meal.name }}</p>
         </li>
-        <li>
-          <p>Restaurant: {{ getRestaurant(activeBooking.mealId)?.name }}</p>
-        </li>
-        <li>
-          <p>
-            Address: {{ getRestaurant(activeBooking.mealId)?.address.street }}
-            {{ getRestaurant(activeBooking.mealId)?.address.number }}
-            {{ getRestaurant(activeBooking.mealId)?.address.city }}
-            {{ getRestaurant(activeBooking.mealId)?.address.postalCode }}
-          </p>
-        </li>
+        <!--        <li>-->
+        <!--          <p>Restaurant: {{ getRestaurant(activeBooking.mealId)?.name }}</p>-->
+        <!--        </li>-->
+        <!--        <li>-->
+        <!--          <p>Address: {{ formatAddress(restaurant.address) }}</p>-->
+        <!--        </li>-->
         <li>
           <p>Order time: {{ activeBooking.orderedTimestamp }}</p>
         </li>
         <li>
           <p>Pick-up deadline: {{ activeBooking.deadlinePickUpTimestamp }}</p>
         </li>
-        <li>
-          <p class="mt-8">Contact: {{ getRestaurant(activeBooking.mealId)?.phoneNumber }}</p>
-        </li>
+        <!--        <li>-->
+        <!--          <p class="mt-8">Contact: {{ getRestaurant(activeBooking.mealId)?.phoneNumber }}</p>-->
+        <!--        </li>-->
       </ul>
     </div>
     <RouterLink to="/bookings">
