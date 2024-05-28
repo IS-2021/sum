41e141de71529f9a6a7054c6bec3412151e8d629
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-28 21:07:36 +0200

    fix: add missing links to footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,24 +1,26 @@
+<script setup lang="ts">
+import type { AppRoutes } from '@/lib/router.js';
+import Logo from '@/components/Logo.vue';
+
+const links: { name: string; to: AppRoutes }[] = [
+  { name: 'Home', to: '/' },
+  { name: 'Restaurants', to: '/restaurants' },
+  { name: 'Favourites', to: '/favourites' },
+  { name: 'Bookings', to: '/bookings' },
+];
+</script>
+
 <template>
-  <footer
-    class="w-full bottom-0 border border-transparent bg-neutral-800 flex items-center justify-left p-10 text-neutral-50"
-  >
-    <div class="flex flex-col items-center text-xs gap-3">
-      <div class="w-16 h-16">
-        <img src="@/assets/logo.svg" alt="Logo" />
-      </div>
-      <p>&copy; 2024</p>
+  <footer class="bottom-0 bg-neutral-800 flex justify-between p-10 text-neutral-50">
+    <div>
+      <Logo class="w-16 h-16 mb-3" />
+      <p class="text-xs text-neutral-400">&copy; 2024</p>
     </div>
-    <div class="flex-grow"></div>
     <div class="flex flex-col w-32">
       <nav>
-        <ul class="flex flex-col items-center gap-3">
-          <li>
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li>
-            <Button as-child>
-              <RouterLink to="/sign-in">Sign in</RouterLink>
-            </Button>
+        <ul class="space-y-3">
+          <li v-for="{ to, name } in links" :key="`footer-${name}`" class="text-right">
+            <RouterLink :to="to">{{ name }}</RouterLink>
           </li>
         </ul>
       </nav>
