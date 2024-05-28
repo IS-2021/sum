dd8ba985c218600fced29d57c0b274cd50a4a8e7
Marcin Szymajda <marcin.szymajda@boat-systems.pl>
2024-05-28 08:51:20 +0200

    add messages when booking input is not valid

--- backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/bookings/BookingInputDTO.java
+++ backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/bookings/BookingInputDTO.java
@@ -1,9 +1,13 @@
 package org.example.sumatyw_backend.bookings;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record BookingInputDTO(
+    @NotBlank(message = "Meal ID can not be empty!")
     UUID mealId,
+    @NotBlank(message = "User ID can not be empty!")
     UUID userId
 ) {
 }
