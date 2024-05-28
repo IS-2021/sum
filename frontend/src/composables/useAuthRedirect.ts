ba1f6f61bb8119d79a898f3c662097aa7286a714
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-28 16:49:23 +0200

    refactor: define default routes per role

--- frontend/src/composables/useAuthRedirect.ts
+++ frontend/src/composables/useAuthRedirect.ts
@@ -2,6 +2,7 @@ import { useUser } from '@/composables/useUser';
 import { watchEffect } from 'vue';
 import { useRouter } from 'vue-router/auto';
 import type { AppRouteNames, AppRoutes } from '@/lib/router';
+import { Role } from '@/lib/api-model';
 
 interface UseAuthRedirectorProps {
   /**
@@ -22,6 +23,12 @@ interface UseAuthRedirectorProps {
   onAuthenticatedRedirect?: () => void;
 }
 
+const defaultRoutesByRole: Record<Role, AppRouteNames> = {
+  ROLE_USER: '/',
+  ROLE_RESTAURANT: '/manage/',
+  ROLE_ADMIN: '/',
+};
+
 /**
  * Redirects user based on their authentication status.
  * Regular users without a complete profile will be redirected to the onboarding page.
@@ -34,7 +41,7 @@ export function useAuthRedirect({
   onAuthenticatedRedirect,
 }: UseAuthRedirectorProps) {
   const router = useRouter();
-  const { isLoaded, isSignedIn, isProfileComplete } = useUser();
+  const { user, isLoaded, isSignedIn, isProfileComplete } = useUser();
 
   async function redirectTo(to: AppRoutes, onRedirect?: () => void) {
     await router.push(to);
@@ -63,7 +70,8 @@ export function useAuthRedirect({
     if (isAuthRouteMatching && !isSignedIn.value) {
       await redirectTo('/sign-in', onGuestRedirect);
     } else if (isGuestRouteMatching && isSignedIn.value) {
-      await redirectTo('/', onAuthenticatedRedirect);
+      const defaultRoute = defaultRoutesByRole[user.value?.role ?? Role.ROLE_USER];
+      await redirectTo(defaultRoute, onAuthenticatedRedirect);
     }
   });
 }
