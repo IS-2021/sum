a4c6b6561e62cb27d3e891f9ef0d947daecdf0ac
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-28 15:53:27 +0200

    fix: change radius values

--- frontend/src/components/restaurants/RadiusSelect.vue
+++ frontend/src/components/restaurants/RadiusSelect.vue
@@ -13,18 +13,19 @@ const radiusModel = defineModel<string | undefined>('radius');
 </script>
 
 <template>
-  <Select v-model="radiusModel" default-value="5">
+  <Select v-model="radiusModel" default-value="2">
     <SelectTrigger class="w-[180px]">
       <SelectValue placeholder="Select radius" />
     </SelectTrigger>
     <SelectContent>
       <SelectGroup>
         <SelectLabel>Restaurants within</SelectLabel>
+        <SelectItem value="1"> 1 km </SelectItem>
         <SelectItem value="2"> 2 km </SelectItem>
+        <SelectItem value="3"> 3 km </SelectItem>
         <SelectItem value="5"> 5 km </SelectItem>
         <SelectItem value="10"> 10 km </SelectItem>
         <SelectItem value="15"> 15 km </SelectItem>
-        <SelectItem value="20"> 20 km </SelectItem>
       </SelectGroup>
     </SelectContent>
   </Select>
