a6429e5a6ed6ff4ae266525505b3692128ab1858
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-30 00:56:07 +0200

    refactor: make form fields dynamically generated

--- frontend/src/pages/manage/onboarding/index.vue
+++ frontend/src/pages/manage/onboarding/index.vue
@@ -24,6 +24,62 @@ const { current, goToPrevious, goToNext, isCurrent } = useStepper([
   'location',
 ]);
 
+const formFields = {
+  details: [
+    {
+      label: 'Restaurant name',
+      fieldName: 'details.name',
+    },
+    {
+      label: 'Phone number',
+      fieldName: 'details.phoneNumber',
+    },
+  ],
+  hours: [
+    {
+      label: 'Monday',
+      fieldName: 'hours.monday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Tuesday',
+      fieldName: 'hours.tuesday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Wednesday',
+      fieldName: 'hours.wednesday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Thursday',
+      fieldName: 'hours.thursday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Friday',
+      fieldName: 'hours.friday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Saturday',
+      fieldName: 'hours.saturday',
+      fields: ['Open', 'Close'],
+    },
+    {
+      label: 'Sunday',
+      fieldName: 'hours.sunday',
+      fields: ['Open', 'Close'],
+    },
+  ],
+  location: {
+    address: 'Address',
+    city: 'City',
+    state: 'State',
+    zip: 'Zip',
+  },
+};
+
 const formSchema = toTypedSchema(restaurantSchema);
 const form = useForm({
   validationSchema: formSchema,
@@ -51,23 +107,20 @@ const onSubmit = form.handleSubmit((values) => {
 <template>
   <div class="grid min-h-svh grid-cols-2">
     <div class=""></div>
-    <div class="grid-rows-5 p-10 md:grid md:h-svh">
+    <div class="space-y-5 p-10 md:h-svh">
       <h1>Restaurant form</h1>
+
       <form @submit="onSubmit" class="space-y-4">
         <!-- Details -->
         <div :class="cn('hidden', isCurrent('details') && 'block')">
-          <FormField v-slot="{ componentField }" name="details.name">
+          <FormField
+            v-for="{ label, fieldName } in formFields.details"
+            :key="fieldName"
+            v-slot="{ componentField }"
+            :name="fieldName"
+          >
             <FormItem>
-              <FormLabel>Restaurant name</FormLabel>
-              <FormControl>
-                <Input type="text" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-          <FormField v-slot="{ componentField }" name="details.phoneNumber">
-            <FormItem>
-              <FormLabel>Phone number</FormLabel>
+              <FormLabel>{{ label }}</FormLabel>
               <FormControl>
                 <Input type="text" v-bind="componentField" />
               </FormControl>
@@ -79,34 +132,43 @@ const onSubmit = form.handleSubmit((values) => {
         <!-- Hours -->
         <div :class="cn('hidden max-w-80', isCurrent('hours') && 'block')">
           <div class="grid grid-cols-[max-content,1fr,1fr] gap-2 gap-x-4">
-            <p class="col-start-2 text-neutral-500">Open</p>
-            <p class="text-neutral-500">Close</p>
+            <p class="col-start-2 text-sm font-medium">Open</p>
+            <p class="text-sm font-medium">Close</p>
 
-            <p aria-hidden="true" class="h-fit self-center">Monday</p>
+            <template v-for="{ label, fieldName } in formFields.hours" :key="fieldName">
+              <p aria-hidden="true" class="h-fit self-center">
+                {{ label }}
+              </p>
 
-            <FormField v-slot="{ componentField }" name="hours.monday[0]">
-              <FormItem class="space-y-0">
-                <FormLabel class="sr-only">Open</FormLabel>
-                <FormControl>
-                  <Input type="text" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="hours.monday[1]">
-              <FormItem class="space-y-0">
-                <FormLabel class="sr-only">Close</FormLabel>
-                <FormControl>
-                  <Input type="text" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
+              <FormField v-slot="{ componentField }" :name="`${fieldName}[0]`">
+                <FormItem class="space-y-0">
+                  <FormLabel class="sr-only">Open</FormLabel>
+                  <FormControl>
+                    <Input type="text" v-bind="componentField" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
+              <FormField v-slot="{ componentField }" :name="`${fieldName}[1]`">
+                <FormItem class="space-y-0">
+                  <FormLabel class="sr-only">Close</FormLabel>
+                  <FormControl>
+                    <Input type="text" v-bind="componentField" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
+            </template>
           </div>
         </div>
 
-        <Button type="submit"> Submit </Button>
+        <!--        <Button type="submit"> Submit </Button>-->
       </form>
+
+      <div class="flex gap-2">
+        <Button @click="goToPrevious"> Previous </Button>
+        <Button @click="goToNext"> Next </Button>
+      </div>
     </div>
   </div>
 </template>
