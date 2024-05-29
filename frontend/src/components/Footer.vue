8276abddac1e0c072a7a086d52d1c4faa5430c65
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-29 17:06:47 +0200

    refactor: have single source of truth for nav links

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import type { AppRoutes } from '@/lib/router.js';
 import Logo from '@/components/Logo.vue';
-
-const links: { name: string; to: AppRoutes }[] = [
-  { name: 'Home', to: '/' },
-  { name: 'Restaurants', to: '/restaurants' },
-  { name: 'Favourites', to: '/favourites' },
-  { name: 'Bookings', to: '/bookings' },
-];
+import { navbarLinks } from '@/components/navbar/links';
 </script>
 
 <template>
-  <footer class="bottom-0 bg-neutral-800 flex justify-between p-10 text-neutral-50">
+  <footer class="bottom-0 flex justify-between bg-neutral-800 p-10 text-neutral-50">
     <div>
-      <Logo class="w-16 h-16 mb-3" />
+      <Logo class="mb-3 h-16 w-16" />
       <p class="text-xs text-neutral-400">&copy; 2024</p>
     </div>
-    <div class="flex flex-col w-32">
+    <div class="flex w-32 flex-col">
       <nav>
         <ul class="space-y-3">
-          <li v-for="{ to, name } in links" :key="`footer-${name}`" class="text-right">
+          <li v-for="{ to, name } in navbarLinks" :key="`footer-${name}`" class="text-right">
             <RouterLink :to="to">{{ name }}</RouterLink>
           </li>
         </ul>
