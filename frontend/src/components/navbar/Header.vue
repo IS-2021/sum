e0beaa8105627460d7144f1d3c2a85f654821259
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-05-29 17:00:35 +0200

    fix: replace dropdown with sheet

--- frontend/src/components/navbar/Header.vue
+++ frontend/src/components/navbar/Header.vue
@@ -1,27 +1,20 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
 import { useUser } from '@/composables/useUser';
-import DropdownMenu from '@/components/navbar/DropdownMenu.vue';
+import MobileNavSidebar from '@/components/navbar/MobileNavSidebar.vue';
 import AppNavLink from '@/components/navbar/AppNavLink.vue';
 import UserActionsDropdown from '@/components/navbar/UserActionsDropdown.vue';
-import type { AppRoutes } from '@/lib/router';
 import Logo from '@/components/Logo.vue';
+import { navbarLinks } from '@/components/navbar/links';
 
 const { isSignedIn } = useUser();
-
-const links: { name: string; to: AppRoutes }[] = [
-  { name: 'Home', to: '/' },
-  { name: 'Restaurants', to: '/restaurants' },
-  { name: 'Favourites', to: '/favourites' },
-  { name: 'Bookings', to: '/bookings' },
-];
 </script>
 
 <template>
   <header
-    class="sticky top-0 z-50 flex h-16 w-full items-center border border-transparent bg-neutral-950 px-6 text-white"
+    class="sticky top-0 z-50 flex h-16 items-center border border-transparent bg-neutral-950 px-6 text-white"
   >
-    <DropdownMenu />
+    <MobileNavSidebar />
 
     <nav class="ml-4 flex flex-grow justify-between sm:ml-0">
       <ul class="flex items-center gap-6">
@@ -29,7 +22,7 @@ const links: { name: string; to: AppRoutes }[] = [
           <Logo class="h-8 w-8" />
         </RouterLink>
 
-        <li class="hidden sm:block" v-for="{ to, name } in links" :key="`navbar-${name}`">
+        <li class="hidden sm:block" v-for="{ to, name } in navbarLinks" :key="`navbar-${name}`">
           <AppNavLink :to="to">{{ name }}</AppNavLink>
         </li>
       </ul>
