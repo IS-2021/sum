f28d0cbfe237c732f4ac7af483636333e938378e
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-05-30 18:24:57 +0200

    fix: homepage last bookings display

--- frontend/src/components/homepage/LatestBookingBody.vue
+++ frontend/src/components/homepage/LatestBookingBody.vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
 import type { BookingDTO } from '@/lib/api-model';
-import { useGetMealsId } from '@/lib/api/meals/meals';
-import { computed, unref } from 'vue';
 
-import LatestBookingrestaurantInfo from '@/components/homepage/LatestBookingRestaurantInfo.vue';
+import RestaurantImageComponent from '@/components/homepage/RestaurantImageComponent.vue';
 
 const props = defineProps<{
   booking: BookingDTO;
 }>();
 
-const { data } = useGetMealsId(props.booking.mealId);
-const meal = computed(() => unref(data)?.data);
+const meal = props.booking.meal;
+const restaurant = props.booking.restaurant;
 </script>
 
 <template>
   <article
     class="group bg-neutral-200 text-neutral-900 rounded text-lg space-y-3 p-4 h-full hover:shadow transition-shadow"
   >
-    <LatestBookingrestaurantInfo
-      v-if="meal"
-      :restaurantId="meal.restaurantId"
-      :mealName="meal.name"
-    />
+    <article v-if="restaurant">
+      <RestaurantImageComponent :imageUrl="restaurant.imageUrl" :restaurantName="restaurant.name" />
+
+      <header class="flex flex-col justify-between items-start">
+        <div class="flex">
+          <p class="font-bold mr-1">Restaurant:</p>
+          <p>{{ restaurant.name }}</p>
+        </div>
+        <div class="flex">
+          <p class="font-bold mr-1">Meal:</p>
+          <p>{{ meal.name }}</p>
+        </div>
+      </header>
+    </article>
   </article>
 </template>
