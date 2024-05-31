bed778e0a102430a3cff20be4f8379eb3b1e1c7f
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-05-31 16:26:42 +0200

    feat: add additonal info to booking and page reload

--- frontend/src/components/(manage)/dashboard/BookingCard.vue
+++ frontend/src/components/(manage)/dashboard/BookingCard.vue
@@ -1,37 +1,75 @@
 <script setup lang="ts">
-import { CircleDashedIcon, Clock4Icon, CircleIcon, CircleCheckBigIcon } from 'lucide-vue-next';
+import {
+  CircleDashedIcon,
+  Clock4Icon,
+  CircleIcon,
+  CircleCheckBigIcon,
+  CircleXIcon,
+  CircleSlash2Icon,
+} from 'lucide-vue-next';
 import { cn } from '@/lib/utils';
 import { useBookingCard } from '@/components/(manage)/dashboard/useBookingCard';
+import type { BookingDTO } from '@/lib/api-model';
+import { Button } from '@/components/ui/button';
+import { useGetUsersId } from '@/lib/api/users/users';
+import { computed, unref } from 'vue';
 
 interface BookingCardProps {
-  mealName: string;
-  pickupAt: Date;
+  booking: BookingDTO;
 }
 
 const props = defineProps<BookingCardProps>();
 
-const { isPending, isPast, remainingTime } = useBookingCard(props.pickupAt);
+const { data } = useGetUsersId(props.booking.userId);
+const client = computed(() => unref(data)?.data);
+
+const emits = defineEmits<{
+  (e: 'onBookingAccept', booking: BookingDTO): void;
+}>();
+
+const { isPending, remainingTime } = useBookingCard(
+  new Date(
+    props.booking.status === 'Active'
+      ? props.booking.deadlinePickUpTimestamp
+      : props.booking.pickedUpTimestamp,
+  ),
+);
 </script>
 
 <template>
   <div
     :class="
       cn(
-        'transition-opacity flex justify-between px-6 py-4 bg-white rounded border border-neutral-300',
-        (isPending || isPast) && 'opacity-50',
+        'transition-opacity px-6 py-4 bg-white rounded border border-neutral-300',
+        (isPending || booking.status !== 'Active') && 'opacity-50',
       )
     "
   >
-    <p class="flex items-center gap-5">
-      <CircleDashedIcon v-if="isPending" />
-      <CircleCheckBigIcon v-else-if="isPast" />
-      <CircleIcon v-else />
-      Hawaian
-    </p>
+    <div class="flex items-center justify-between mb-4">
+      <p class="flex items-center gap-5">
+        <Button
+          size="icon"
+          variant="ghost"
+          @click="emits('onBookingAccept', booking)"
+          :disabled="booking.status !== 'Active'"
+        >
+          <CircleDashedIcon v-if="isPending" />
+          <CircleCheckBigIcon v-else-if="booking.status === 'PickedUp'" />
+          <CircleXIcon v-else-if="booking.status === 'Cancelled'" />
+          <CircleIcon v-else-if="booking.status === 'Active'" />
+          <CircleSlash2Icon v-else-if="booking.status === 'OutOfDate'" />
+        </Button>
+        {{ booking.meal.name }}
+      </p>
 
-    <p class="flex gap-2">
-      <span class="font-semibold">{{ remainingTime }}</span>
-      <Clock4Icon />
-    </p>
+      <p v-if="booking.status === 'Active' || booking.status === 'PickedUp'" class="flex gap-2">
+        <span class="font-semibold">{{ remainingTime }}</span>
+        <Clock4Icon />
+      </p>
+    </div>
+    <div v-if="client">
+      <p>Client: {{ client.firstName }} {{ client.secondName }}</p>
+      <p>Phone: {{ client.phoneNumber }}</p>
+    </div>
   </div>
 </template>
