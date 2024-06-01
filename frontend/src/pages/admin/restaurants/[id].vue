7f378d5e9d076ab0562c6ae4234c078bb8da5a24
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-01 19:34:16 +0200

    feat: add ability to activate restaurants

--- frontend/src/pages/admin/restaurants/[id].vue
+++ frontend/src/pages/admin/restaurants/[id].vue
@@ -7,7 +7,12 @@ meta:
 import { useHead } from '@unhead/vue';
 import { useRoute } from 'vue-router/auto';
 import { computed } from 'vue';
-import { useGetAdminRestaurantsId } from '@/lib/api/admin-restaurants/admin-restaurants';
+import {
+  putAdminRestaurantsId,
+  useGetAdminRestaurantsId,
+} from '@/lib/api/admin-restaurants/admin-restaurants';
+import RestaurantStatus from '@/components/(manage)/common/RestaurantStatus.vue';
+import { Button } from '@/components/ui/button';
 
 useHead({
   title: 'Restaurant',
@@ -16,12 +21,31 @@ useHead({
 const route = useRoute('/admin/restaurants/[id]');
 const restaurantId = route.params.id;
 
-const { data } = useGetAdminRestaurantsId(restaurantId);
+const { data, refetch } = useGetAdminRestaurantsId(restaurantId);
 const restaurant = computed(() => data?.value?.data);
+
+async function handleRestaurantActivate() {
+  const res = await putAdminRestaurantsId(restaurantId);
+
+  if (res.status === 200) {
+    await refetch();
+  }
+}
 </script>
 
 <template>
-  <div v-if="restaurant">
-    <h1 class="text-2xl font-semibold tracking-tight mb-10">{{ restaurant.name }}</h1>
+  <div v-if="restaurant" class="max-w-screen-md">
+    <h1 class="mb-10 text-2xl font-semibold tracking-tight">{{ restaurant.name }}</h1>
+
+    <div
+      v-if="restaurant.status === 'Inactive'"
+      class="flex items-center justify-between rounded border border-yellow-500 bg-yellow-50 px-4 py-2"
+    >
+      <RestaurantStatus :status="restaurant.status" />
+
+      <Button @click="handleRestaurantActivate" class="bg-yellow-500 hover:bg-yellow-500/80">
+        Activate
+      </Button>
+    </div>
   </div>
 </template>
