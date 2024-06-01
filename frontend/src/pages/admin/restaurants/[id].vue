d0fff2612d7268c988ac3dd95ee418ede4d9a09a
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-01 18:00:31 +0200

    feat: add single restaurant page

---
+++ frontend/src/pages/admin/restaurants/[id].vue
@@ -0,0 +1,27 @@
+<route lang="yaml">
+meta:
+  layout: manage
+</route>
+
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+import { useRoute } from 'vue-router/auto';
+import { computed } from 'vue';
+import { useGetRestaurantsId } from '@/lib/api/restaurants/restaurants';
+
+useHead({
+  title: 'Restaurant',
+});
+
+const route = useRoute('/admin/restaurants/[id]');
+const restaurantId = route.params.id;
+
+const { data } = useGetRestaurantsId(restaurantId);
+const restaurant = computed(() => data?.value?.data);
+</script>
+
+<template>
+  <div v-if="restaurant">
+    <h1 class="text-2xl font-semibold tracking-tight mb-10">{{ restaurant.name }}</h1>
+  </div>
+</template>
