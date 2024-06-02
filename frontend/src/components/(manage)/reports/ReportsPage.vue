8d89ad2899d6dbe29b6a5c490a28ad6126857c75
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-06-02 13:35:50 +0200

    fix: minor update

--- frontend/src/components/(manage)/reports/ReportsPage.vue
+++ frontend/src/components/(manage)/reports/ReportsPage.vue
@@ -19,7 +19,7 @@ const reports = computed(() => unref(data)?.data);
     v-if="reports"
     class="space-y-5 p-4 bg-neutral-100 border border-neutral-200 max-w-screen-md flex-shrink"
   >
-    <h2 class="font-semibold">Ratings {{ reports.length }}</h2>
+    <h2 class="font-semibold">Ratings ({{ reports.length }})</h2>
     <p class="mt-4" v-if="reports.length === 0">No reports found</p>
     <ul v-else class="space-y-2">
       <li v-for="report in reports" v-bind:key="report.id">
