a15b88637646e2f428a4beaf95e4bf01e663122b
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-02 14:03:48 +0200

    feat: add admin user pages

---
+++ frontend/src/pages/admin/users/[id].vue
@@ -0,0 +1,22 @@
+<route lang="yaml">
+meta:
+  layout: admin
+</route>
+
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+import { useRoute } from 'vue-router/auto';
+
+useHead({
+  title: 'User',
+});
+
+const route = useRoute('/admin/restaurants/[id]');
+const userId = route.params.id;
+</script>
+
+<template>
+  <div>
+    <h1 class="mb-10 text-2xl font-semibold tracking-tight">Username</h1>
+  </div>
+</template>
