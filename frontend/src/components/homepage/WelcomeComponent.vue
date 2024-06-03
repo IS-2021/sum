540b7ecde8ef37de449fd67ab8651468ea208584
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-06-03 16:28:29 +0200

    fix: restyle welcome component

--- frontend/src/components/homepage/WelcomeComponent.vue
+++ frontend/src/components/homepage/WelcomeComponent.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import type { UserMeDTO } from '@/lib/api-model';
-
-import CompletedBookingsCount from './CompletedBookingsCount.vue';
 import { useGetBookings } from '@/lib/api/bookings/bookings';
 import { computed, unref } from 'vue';
 
 import Logo from '../Logo.vue';
+import { cn } from '@/lib/utils';
 
 const props = defineProps<{
   user: UserMeDTO;
 }>();
 
 const { data } = useGetBookings({ userId: props.user.id });
-const bookings = computed(() => unref(data)?.data);
+const bookings = computed(() => unref(data)?.data ?? []);
+
+const completedBookingsCount = computed(
+  () => bookings.value.filter((booking) => booking.status === 'PickedUp').length,
+);
 </script>
 
 <template>
-  <div
-    class="flex rounded-md mb-8 bg-secondary px-8 py-4 justify-center sm:justify-between flex-wrap"
-  >
-    <div class="rounded-md flex flex-col justify-center items-start">
-      <p v-if="user" class="text-2xl font-bold text-center">Welcome {{ props.user.firstName }}!</p>
-      <CompletedBookingsCount v-if="bookings" :bookings="bookings" />
+  <div class="mb-8 rounded-md bg-secondary p-8 py-6">
+    <div class="mb-2.5 flex flex-col items-start justify-center rounded-md">
+      <p v-if="user" class="text-2xl font-bold">Welcome, {{ props.user.firstName }}!</p>
     </div>
-    <div class="flex gap-1 items-end ml-8 mt-8">
-      <h1 class="text-primary">FoodGood team</h1>
-      <Logo class="h-4 w-4 mb-1" />
+    <div
+      :class="
+        cn(
+          'flex flex-wrap items-center gap-3',
+          completedBookingsCount > 0 ? 'justify-between' : 'justify-end',
+        )
+      "
+    >
+      <p class="text-lg" v-if="completedBookingsCount > 0">
+        You saved {{ completedBookingsCount }} meals so far!
+      </p>
+
+      <div class="flex items-center gap-2 self-end">
+        <p>FoodGood team</p>
+        <Logo class="size-5 fill-accent" />
+      </div>
     </div>
   </div>
 </template>
