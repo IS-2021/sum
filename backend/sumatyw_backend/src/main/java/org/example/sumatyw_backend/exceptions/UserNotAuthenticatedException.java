3b4a987e5a5877f2508391b2c16b2bc02ffe8abb
j-osypiuk <kuba6535@gmail.com>
2024-06-10 20:34:21 +0200

    Add exception handler to UserNotAuthenticatedException

--- backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/exceptions/UserNotAuthenticatedException.java
+++ backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/exceptions/UserNotAuthenticatedException.java
@@ -1,9 +1,5 @@
 package org.example.sumatyw_backend.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.UNAUTHORIZED, reason = "User is not authenticated")
 public class UserNotAuthenticatedException extends RuntimeException {
     public UserNotAuthenticatedException(String message) {
         super(message);
--- backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/exceptions/advice/ApplicationExceptionHandler.java
+++ backend/sumatyw_backend/src/main/java/org/example/sumatyw_backend/exceptions/advice/ApplicationExceptionHandler.java
@@ -1,9 +1,6 @@
 package org.example.sumatyw_backend.exceptions.advice;
 
-import org.example.sumatyw_backend.exceptions.InvalidDataException;
-import org.example.sumatyw_backend.exceptions.ObjectNotFoundException;
-import org.example.sumatyw_backend.exceptions.ResourceAlreadyExistsException;
-import org.example.sumatyw_backend.exceptions.UserNotFoundException;
+import org.example.sumatyw_backend.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -55,6 +52,15 @@ public class ApplicationExceptionHandler {
             HttpStatus.NOT_FOUND, e.getMessage()
         );
         problemDetail.setTitle(HttpStatus.NOT_FOUND.getReasonPhrase());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(UserNotAuthenticatedException.class)
+    public ProblemDetail handleUserNotAuthenticatedException(UserNotAuthenticatedException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+            HttpStatus.UNAUTHORIZED, e.getMessage()
+        );
+        problemDetail.setTitle(HttpStatus.UNAUTHORIZED.getReasonPhrase());
         return problemDetail;
     }
 }
